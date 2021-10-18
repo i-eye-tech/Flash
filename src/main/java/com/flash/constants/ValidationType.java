9872ff889cc22ea5eeb79167657eeeeb6a4abf4f
Avdhesh Gupta <avdheshgupta@NOVI-C02G22DDMD6M.local>
2021-10-18 18:07:34 +0530

    code push change

--- src/main/java/com/flash/constants/ValidationType.java
+++ src/main/java/com/flash/constants/ValidationType.java
@@ -10,9 +10,6 @@ public enum ValidationType {
         switch (this){
             case COMPARATOR: return GenericTest.class;
             case DBVALIDATOR: return DbValidationTest.class;
-//            case CATALOGTEST: return CatalogTest.class;
-//            case CHECKOUT: return CheckoutTest.class;
-//            case SUBSCRIPTION_ENGINE: return SubscriptionTest.class;
             case GENERICVALIDATOR: return GenericValidationTest.class;
         }
         return null;
